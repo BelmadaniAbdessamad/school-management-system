65faa6165da143a919f73aebb33b07b33f6dd5bc
FUJITSU <FUJITSU@DESKTOP-IV0DDR2>
2024-03-25 02:20:07 +0000

    Created the servlet reponsible for get all student

---
+++ src/main/java/com/sms/service/DefaultService.java
@@ -0,0 +1,118 @@
+package com.sms.service;
+
+import java.util.List;
+
+import com.sms.beans.Departement;
+import com.sms.beans.Etudiant;
+import com.sms.beans.Filiere;
+import com.sms.persistence.MySqlPersistence;
+import com.sms.persistence.Persistence;
+
+public class DefaultService implements Service {
+	
+	private Persistence persistence;
+	static Service defaultService = null;
+	
+	public static Service getServiceInstance() {
+		if( defaultService == null) {
+			defaultService = new DefaultService(new MySqlPersistence());
+		}
+		
+		return defaultService;
+	}
+
+	public DefaultService(Persistence persistence) {
+		super();
+		this.persistence = persistence;
+	}
+
+	@Override
+	public List<Etudiant> getAllStudents() {
+		// TODO Auto-generated method stub
+		List<Etudiant> etudiants = persistence.getAllStudents();
+		if(etudiants != null && etudiants.size() >=1) {
+			return etudiants;
+		}
+		return null;
+	}
+
+	@Override
+	public Etudiant findStudentByFullName(String fullName) {
+		// TODO Auto-generated method stub
+		if( !fullName.isBlank()) return persistence.findStudentByFullName(fullName);
+		return null;
+	}
+
+	@Override
+	public boolean insertStudent(Etudiant et) {
+		// TODO Auto-generated method stub
+		if(et != null && et.attributsAreValid()) {
+			return persistence.insertStudent(et);
+		}
+		return false;
+	}
+
+	@Override
+	public boolean deleteStudent(int id) {
+		// TODO Auto-generated method stub
+		return persistence.deleteStudent(id);
+	}
+
+	@Override
+	public boolean updateStudent(int id, Etudiant newData) {
+		return false;
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Filiere> getAllMajors() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean insertMajor(Filiere fl) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void deleteMajor(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean updateMajor(int id, Filiere newData) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Departement> getAllDepartements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean insertDepartement(Departement depart) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void deleteDepartement(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean updateDepartement(int id, Departement newData) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	 
+
+}
---
+++ src/main/java/com/sms/service/Service.java
@@ -0,0 +1,27 @@
+package com.sms.service;
+
+import java.util.List;
+
+import com.sms.beans.Departement;
+import com.sms.beans.Etudiant;
+import com.sms.beans.Filiere;
+
+public interface Service {
+	
+	List<Etudiant> getAllStudents();
+	Etudiant findStudentByFullName(String fullName);
+	boolean insertStudent(Etudiant et);
+	boolean deleteStudent(int id);
+	boolean updateStudent(int id,Etudiant newData);
+	
+	List<Filiere> getAllMajors();
+	boolean insertMajor(Filiere fl);
+	void deleteMajor(int id);
+	boolean updateMajor(int id,Filiere newData);
+	
+	List<Departement> getAllDepartements();
+	boolean insertDepartement(Departement depart);
+	void deleteDepartement(int id);
+	boolean updateDepartement(int id,Departement newData);
+
+}
