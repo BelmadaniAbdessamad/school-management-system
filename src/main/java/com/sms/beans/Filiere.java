e4abc1dcf963a5a01f5c07a197ccc001e90c0a19
FUJITSU <FUJITSU@DESKTOP-IV0DDR2>
2024-03-31 04:20:25 +0000

    Added id field to Major class - with getters and setters and updated the code

--- src/main/java/com/sms/beans/Filiere.java
+++ src/main/java/com/sms/beans/Filiere.java
@@ -4,17 +4,26 @@ public class Filiere {
 	
 	private String nom;
 	private Departement departement;
-	
+	private int id;
 	
 	
 	public Filiere() {
 		super();
 	}
-	public Filiere(String nom, Departement departement) {
+
+	public Filiere(String nom, Departement departement, int id) {
 		super();
 		this.nom = nom;
 		this.departement = departement;
+		this.id = id;
 	}
+
+	public Filiere(String nom, int id) {
+		super();
+		this.nom = nom;
+		this.id = id;
+	}
+
 	public String getNom() {
 		return nom;
 	}
@@ -26,6 +35,12 @@ public class Filiere {
 	}
 	public void setDepartement(Departement departement) {
 		this.departement = departement;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
 	}
 	
 	
