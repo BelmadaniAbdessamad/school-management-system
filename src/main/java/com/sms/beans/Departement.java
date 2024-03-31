ec869a5dc9b13ac7b474c8ff831aaf5c499b3fcc
FUJITSU <FUJITSU@DESKTOP-IV0DDR2>
2024-03-31 23:18:53 +0000

    Added majorCount & studentCount & id fields to department bean & their getters and setters and toString method

--- src/main/java/com/sms/beans/Departement.java
+++ src/main/java/com/sms/beans/Departement.java
@@ -3,7 +3,9 @@ package com.sms.beans;
 public class Departement {
 	
 	private String nom;
-    
+    private int id;
+    private int filiereCount;
+    private int EtudiantCount;
 	
 	public Departement() {
 		super();
@@ -21,7 +23,39 @@ public class Departement {
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-	
-	
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getFiliereCount() {
+		return filiereCount;
+	}
+
+	public void setFiliereCount(int filiereCount) {
+		this.filiereCount = filiereCount;
+	}
+
+	public int getEtudiantCount() {
+		return EtudiantCount;
+	}
+
+	public void setEtudiantCount(int etudiantCount) {
+		EtudiantCount = etudiantCount;
+	}
+
+	@Override
+	public String toString() {
+		return "Departement [nom=" + nom + ", id=" + id + ", filiereCount=" + filiereCount + ", EtudiantCount="
+				+ EtudiantCount + "]";
+	}
+	
+	
+	
+	
+   
 }
