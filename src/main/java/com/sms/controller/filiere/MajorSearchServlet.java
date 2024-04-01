fdaa654983533728166f90e79cf19d970dc76405
FUJITSU <FUJITSU@DESKTOP-IV0DDR2>
2024-04-01 02:28:17 +0000

    Major search servlet is ready

--- src/main/java/com/sms/controller/filiere/MajorSearchServlet.java
+++ src/main/java/com/sms/controller/filiere/MajorSearchServlet.java
@@ -7,9 +7,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import com.sms.beans.Departement;
+import com.sms.beans.Filiere;
+import com.sms.service.DefaultService;
+
 /**
  * Servlet implementation class MajorSearchServlet
  */
+@WebServlet("/find-major")
 public class MajorSearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -25,8 +30,23 @@ public class MajorSearchServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String searchQuery = request.getParameter("search");
+		Filiere foundFl =null;
+		try {
+			int searchQueryId = Integer.parseInt(searchQuery);
+			foundFl = DefaultService.getServiceInstance().findMajorById(searchQueryId);
+		} catch (Exception e) {
+			// TODO: handle exception
+			foundFl = DefaultService.getServiceInstance().findMajorByName(searchQuery);
+		}
+		
+		
+		if(foundFl != null) {
+			request.setAttribute("found-major-id", foundFl.getId());
+		}else {
+			request.setAttribute("found-major-id", -1);
+		}
+		request.getRequestDispatcher("get-majors").forward(request, response);
 	}
 
 	/**
