a29bb1457261b8952ca04db31e4dbc736174238c
FUJITSU <FUJITSU@DESKTOP-IV0DDR2>
2024-04-18 22:35:15 +0100

    the login servlet logic is done and tested successfully

--- src/main/java/com/sms/controller/Auth/LoginServlet.java
+++ src/main/java/com/sms/controller/Auth/LoginServlet.java
@@ -7,32 +7,58 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import com.sms.service.Auth;
+
 /**
  * Servlet implementation class LoginServlet
  */
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		System.out.print(username+"  "+password);
+		String messages = null;
+		if (username.isBlank() || password.isEmpty()) {
+			messages = "username & password are required";
+			request.setAttribute("messages", messages);
+			request.getRequestDispatcher("index.jsp").forward(request, response);
+			return;
+		}
+
+		if (Auth.login(username, password)) {
+			
+			request.getRequestDispatcher("get-students").forward(request, response);
+		} else {
+			messages = "Internal server error , Please try again later";
+			request.setAttribute("messages", messages);
+			request.getRequestDispatcher("index.jsp").forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
